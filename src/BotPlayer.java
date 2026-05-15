34b59126de2fc0df84cfafb8ead597ee184b543e
dachitke <92733359+dachitke@users.noreply.github.com>
2026-05-15 18:42:10 +0400

    feat:add new human and bot player  classes

---
+++ src/BotPlayer.java
@@ -0,0 +1,11 @@
+public class BotPlayer extends Player {
+
+    public BotPlayer(String name) {
+        super(name);
+    }
+
+    public int chooseMove(Card upCard) {
+        // bot logic
+        return -1;
+    }
+}
---
+++ src/HumanPlayer.java
@@ -0,0 +1,11 @@
+public class HumanPlayer extends Player {
+
+    public HumanPlayer(String name) {
+        super(name);
+    }
+
+    public int chooseMove() {
+        // scanner logic here
+        return 0;
+    }
+}
